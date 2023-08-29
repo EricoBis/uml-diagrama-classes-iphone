a92357dddb30c7b2bd292a5b6fac2329dc0304c7
EricoBis <ericobis.santos@gmail.com>
2023-08-29 13:56:52 -0300

    add | internet folder, NavegadorInternet interface and IPod class

---
+++ internet/NavegadorInternet.java
@@ -0,0 +1,7 @@
+package internet;
+
+public interface NavegadorInternet {
+    void exibirPagina();
+    void adicionarNovaAba();
+    void atualizarPagina();
+}
---
+++ internet/Safari.java
@@ -0,0 +1,21 @@
+package internet;
+
+public class Safari implements NavegadorInternet {
+
+    @Override
+    public void exibirPagina() {
+        System.out.println("Exibindo página");
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+        System.out.println("Adicionando nova aba");
+    }
+
+    @Override
+    public void atualizarPagina() {
+        System.out.println("Atualizando página");
+
+    }
+
+}
