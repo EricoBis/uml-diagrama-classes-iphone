29008d04245c1393ffd49245d00a8a6b5599c183
EricoBis <ericobis.santos@gmail.com>
2023-08-29 13:46:07 -0300

    add | audioplayer

---
+++ audioplayer/IPod.java
@@ -0,0 +1,20 @@
+package audioplayer;
+
+public class IPod implements ReprodutorMusical {
+
+    @Override
+    public void tocar() {
+        System.out.println("Tocando a música");
+    }
+
+    @Override
+    public void pausar() {
+        System.out.println("Música pausada");
+    }
+
+    @Override
+    public void selecionarMusica() {
+        System.out.println("Selecionando a música");
+    }
+    
+}
